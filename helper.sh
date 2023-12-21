34223540d985b72f6d347aaa21ab6fdcfac25e77
lyaguxa <maccree.a@yandex.ru>
2023-12-21 12:19:32 +0300

    деплой

--- helper.sh
+++ helper.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+#!/bin/bash
+
+if [[ $1 = 'config' ]]; then
+
+
+    # cat .env.example >> kernel/.env
+    echo ".env создан"
+    echo "Обязательно смените SECRET_KEY"
+    echo "Так же не забудьте настроить SMTP и SMS"
+fi
+
+if [[ $1 = 'deploy' ]]; then
+	
+    docker-compose up -d --build kernel
+
+fi
