566a8a19eb70280914271ea64061bd9eeb8e31e9
lyaguxa <maccree.a@yandex.ru>
2023-12-21 17:51:16 +0300

    update workflow

--- helper.sh
+++ helper.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#!/bin/bash
 
 if [[ $1 = 'config' ]]; then
 
-
-    # cat .env.example >> kernel/.env
+    rm -rf kernel/.devcontainer
+    rm -rf kernel/.vscode
+    rm -rf README.md
+    cat .env.example >> kernel/.env
     echo ".env создан"
     echo "Обязательно смените SECRET_KEY"
     echo "Так же не забудьте настроить SMTP и SMS"
