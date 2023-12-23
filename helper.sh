fed7512df783741fe70e70efce98e7dab1db4f60
lyaguxa <maccree.a@yandex.ru>
2023-12-23 13:41:40 +0300

    автодеплой и nginx

--- helper.sh
+++ helper.sh
@@ -5,12 +5,11 @@ if [[ $1 = 'config' ]]; then
 
     cat .env.example >> kernel/.env
     echo ".env создан"
-    echo "Обязательно смените SECRET_KEY"
     echo "Так же не забудьте настроить SMTP и SMS"
 fi
 
 if [[ $1 = 'deploy' ]]; then
-	
-    docker-compose up -d --build kernel
+    
+    docker-compose up -d --build 
 
 fi
