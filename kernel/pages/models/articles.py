6f8a25635be725963b11db9f83c7a38d8a9568a3
adrian <maccree.a@yandex.ru>
2023-12-26 14:20:04 +0300

    апдейт адпинки

--- kernel/pages/models/articles.py
+++ kernel/pages/models/articles.py
@@ -2,23 +2,26 @@
 
 from django.db import models
 
-
 class Articles(models.Model):
-    article_name = models.CharField(max_length=255)
-    cover = models.ImageField(upload_to='article_covers/')
-    article_text = models.TextField()
-    quote = models.TextField()
-    quote_image = models.ImageField(upload_to='quote_images/')
+    ARTICLE_TYPE_CHOICES = [
+        ('статья', 'Статья'),
+        ('инновация', 'Инновация'),
+    ]
 
-    drug_url = models.URLField()
-    drug = models.ManyToManyField('pages.Drug', related_name='articles')
-    final_content = models.CharField(max_length=255)
+    article_name = models.CharField(max_length=1024, blank=True, verbose_name='Заголовок')
+    cover = models.ImageField(upload_to='article_covers/', blank=True, null=True)
+    article_text = models.TextField(blank=True)
+    quote = models.TextField(blank=True)
+    quote_image = models.ImageField(upload_to='quote_images/', blank=True, null=True)
 
-    access_number = models.CharField(max_length=255, verbose_name="Поле для добавления расшифровок и номеров одобрения")
-    speciality = models.ManyToManyField('pages.Specialty', related_name='articles')
-    article_type = models.CharField(max_length=255)
-    # saved_data = models.ForeignKey("pages.SavedByUser", on_delete=models.CASCADE, null=True)
+    drug_url = models.URLField(null=True, blank=True)
+    drug = models.ManyToManyField('pages.Drug', related_name='articles', blank=True)
+    final_content = models.CharField(max_length=255, blank=True)
 
+    access_number = models.CharField(max_length=255, verbose_name="Поле для добавления расшифровок и номеров одобрения", blank=True)
+    speciality = models.ManyToManyField('pages.Specialty', related_name='articles', blank=True)
+    article_type = models.CharField(max_length=255, choices=ARTICLE_TYPE_CHOICES, blank=True)
 
     class Meta:
+        verbose_name = "Статья"
         verbose_name_plural = "Статьи"
