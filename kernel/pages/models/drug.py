636654cac46980cc22963b92aeacabd0c691aa30
adrian <maccree.a@yandex.ru>
2023-12-27 06:09:31 +0300

    практика применения

--- kernel/pages/models/drug.py
+++ kernel/pages/models/drug.py
@@ -2,36 +2,14 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from ckeditor.fields import RichTextField
 
-def validate_icon_count(value):
-    if value.count() > 4:
-        raise ValidationError('Выбрано слишком много иконок. Выберите не более 4.')
-
-class Drug(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Название препарата")
-    brief_info = models.TextField(verbose_name="Краткое описание препарата")
-    image = models.ImageField(upload_to='drug_images/', verbose_name="Изображение препарата")
-    instruction_text = RichTextField(verbose_name="Текст инструкции")
-    application_practice = RichTextField(verbose_name="Практика применения")
-    approvals_and_decodings = RichTextField(verbose_name="Расшифровки и номера одобрения")
-    icons = models.ManyToManyField('Icon', blank=True, related_name='drugs', verbose_name="Иконки")
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Препарат'
-        verbose_name_plural = 'Препараты'
-
-
 class Icon(models.Model):
     IMAGE_TYPES = (
         ('image', 'Image'),
         ('svg', 'SVG'),
     )
 
-    drug = models.ForeignKey(Drug, on_delete=models.CASCADE,
+    drug = models.ForeignKey('Drug', on_delete=models.CASCADE,
                     related_name='icons_set', verbose_name="Препарат")
-
 
     image_file = models.FileField(upload_to='icon_files/',
             verbose_name="Иконка",
@@ -39,3 +17,20 @@ class Icon(models.Model):
 
     def __str__(self):
         return str(self.image_file)
+
+class Drug(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название препарата")
+    brief_info = models.TextField(verbose_name="Краткое описание препарата")
+    image = models.ImageField(upload_to='drug_images/', verbose_name="Изображение препарата")
+    instruction_text = RichTextField(verbose_name="Текст инструкции")
+    application_practice_articles = models.ManyToManyField('pages.Articles', blank=True, related_name='application_practice_articles', verbose_name="Статьи в практике применения")
+    application_practice_videos = models.ManyToManyField('pages.VideoLectures', blank=True, related_name='application_practice_videos', verbose_name="Видео в практике применения")
+    approvals_and_decodings = RichTextField(verbose_name="Расшифровки и номера одобрения")
+    icons = models.ManyToManyField(Icon, blank=True, related_name='drugs', verbose_name="Иконки")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Препарат'
+        verbose_name_plural = 'Препараты'
