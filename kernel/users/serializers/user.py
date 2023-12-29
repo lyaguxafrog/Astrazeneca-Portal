c8aa46ef2ff19b9add3ae867fe8d7dff17cf4f85
Adrian Makridenko <maccree.a@yandex.ru>
2023-12-29 15:40:13 +0300

    Update user.py

--- kernel/users/serializers/user.py
+++ kernel/users/serializers/user.py
@@ -1,19 +1,23 @@
-# -*- coding: utf-8 -*-
-
-
-from rest_framework import serializers
-from users.models import UserProfile
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from pages.models import Specialty
+from users.models import UserProfile
+from users.serializers import UserProfileSerializer
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    specialty_id = serializers.PrimaryKeyRelatedField(
-        queryset=Specialty.objects.all(), source='specialty', write_only=True)
-
-    class Meta:
-        model = UserProfile
-        fields = ['temporary_token', 'specialty_id']
-
-    def create(self, validated_data):
-        specialty_id = validated_data.pop('specialty_id')
-        validated_data['specialty'] = specialty_id
-        return super().create(validated_data)
+class UserRegistrationView(APIView):
+    def post(self, request, *args, **kwargs):
+        if 'temporary_token' in request.data and 'specialty_id' in request.data:
+            serializer = UserProfileSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        elif 'temporary_token' in request.data:
+            try:
+                user_profile = UserProfile.objects.get(temporary_token=request.data['temporary_token'])
+                return Response({'user_id': user_profile.id}, status=status.HTTP_200_OK)
+            except UserProfile.DoesNotExist:
+                return Response("User profile not found", status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response("Invalid request data", status=status.HTTP_400_BAD_REQUEST)
