742d358f78b1037b1fc78a69d600e853dbc7ed0d
adrian <maccree.a@yandex.ru>
2023-12-30 11:49:30 +0300

    миграции

--- kernel/pages/views/search.py
+++ kernel/pages/views/search.py
@@ -1,11 +1,42 @@
-# -*- coding: utf-8 -*-
+from itertools import chain
 
 from django.shortcuts import render
-from haystack.query import SearchQuerySet
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views import View
 
-def search(request):
-    query = request.GET.get('q', '')
-    results = SearchQuerySet().filter(content=query)
+from pages.models import Articles, VideoLectures, Drug, Events
 
-    return render(request,
-            'search_results.html', {'results': results, 'query': query})
+
+class ESearchView(View):
+    template_name = 'search/index.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+
+        q = request.GET.get('q')
+        if q:
+            query_sets = []  # Total QuerySet
+
+            # Searching for all models
+            query_sets.append(Articles.objects.search(query=q))
+            query_sets.append(VideoLectures.objects.search(query=q))
+            query_sets.append(Drug.objects.search(query=q))
+            query_sets.append(Events.objects.search(query=q))
+
+            # and combine results
+            final_set = list(chain(*query_sets))
+            final_set.sort(key=lambda x: x.pub_date, reverse=True)  # Sorting
+
+            context['last_question'] = '?q=%s' % q
+
+            current_page = Paginator(final_set, 10)
+
+            page = request.GET.get('page')
+            try:
+                context['object_list'] = current_page.page(page)
+            except PageNotAnInteger:
+                context['object_list'] = current_page.page(1)
+            except EmptyPage:
+                context['object_list'] = current_page.page(current_page.num_pages)
+
+        return render(request=request, template_name=self.template_name, context=context)
