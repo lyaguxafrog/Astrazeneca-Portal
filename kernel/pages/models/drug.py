8d615c6239f55ab4477c2ff8e772b1d55ced25ca
adrian <maccree.a@yandex.ru>
2024-01-04 19:07:11 +0300

    drugs images

--- kernel/pages/models/drug.py
+++ kernel/pages/models/drug.py
@@ -5,6 +5,13 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from ckeditor.fields import RichTextField
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from pathlib import Path
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
+
 
 class Icon(models.Model):
     IMAGE_TYPES = (
@@ -44,9 +51,61 @@ class Drug(models.Model):
     url_field = models.URLField(verbose_name="Ссылка на интрукцию в PDF", null=True, blank=True)
     file_field = models.FileField(verbose_name="Инструкция в формате PDF", null=True, blank=True, upload_to='pdf_files/')
 
+    image_1400px = models.ImageField(upload_to='drugs/1400px/', verbose_name="Изображение 1400px", null=True, blank=True)
+    image_2800px = models.ImageField(upload_to='drugs/2800px/', verbose_name="Изображение 2800px", null=True, blank=True)
+    image_390px = models.ImageField(upload_to='drugs/390px/', verbose_name="Изображение 390px", null=True, blank=True)
+    image_780px = models.ImageField(upload_to='drugs/780px/', verbose_name="Изображение 780px", null=True, blank=True)
+
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = 'Препарат'
         verbose_name_plural = 'Препараты'
+
+
+class DisableSignals:
+    def __init__(self, sender):
+        self.sender = sender
+        self._receivers = None
+
+    def __enter__(self):
+        self._receivers = pre_save.receivers
+        pre_save.receivers = []
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pre_save.receivers = self._receivers
+
+# Обработчик сигнала pre_save
+@receiver(pre_save, sender=Drug)
+def process_video_cover(sender, instance, **kwargs):
+    if instance.image:
+        file_path = instance.image.path
+
+        # Проверяем, существует ли файл
+        if Path(file_path).exists():
+            # Отключаем сигналы для избежания рекурсии
+            with DisableSignals(sender=Drug):
+                image = Image.open(file_path)
+
+                # Создаем байтовый поток и сохраняем изображения с разными размерами
+                image_stream_1400px = BytesIO()
+                image.resize((1400, 1400)).save(image_stream_1400px, format='JPEG')
+                instance.image_1400px.save(f"{instance.image.name}_1400px.jpg", File(image_stream_1400px), save=False)
+
+                image_stream_2800px = BytesIO()
+                image.resize((2800, 2800)).save(image_stream_2800px, format='JPEG')
+                instance.image_2800px.save(f"{instance.image.name}_2800px.jpg", File(image_stream_2800px), save=False)
+
+                image_stream_390px = BytesIO()
+                image.resize((390, 390)).save(image_stream_390px, format='JPEG')
+                instance.image_390px.save(f"{instance.image.name}_390px.jpg", File(image_stream_390px), save=False)
+
+                image_stream_780px = BytesIO()
+                image.resize((780, 780)).save(image_stream_780px, format='JPEG')
+                instance.image_780px.save(f"{instance.image.name}_780px.jpg", File(image_stream_780px), save=False)
+
+                # Сохраняем изменения в модели вручную
+                instance.save()
+        else:
+            print(f"Файл не найден: {file_path}")
