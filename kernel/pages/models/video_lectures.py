0896578aea64ac45b0e9a9e5387263312cef71d9
adrian <maccree.a@yandex.ru>
2024-01-06 22:10:49 +0300

    fix модеелей

--- kernel/pages/models/video_lectures.py
+++ kernel/pages/models/video_lectures.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from distutils.command import upload
+from tkinter.tix import Tree
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from ckeditor.fields import RichTextField
@@ -22,17 +24,24 @@ class VideoLectures(models.Model):
     short_description = models.TextField(verbose_name='Краткое описание')
     conspect = RichTextField(verbose_name="Конспект видео")
     video = models.FileField(upload_to='video_lectures/', verbose_name='Видео')
-    video_cover = models.ImageField(upload_to='video_covers/', verbose_name="Обложка видео")
+    video_cover_desktop = models.ImageField(upload_to='video_covers/', verbose_name="Обложка видео, десктоп", blank=True, null=True)
+    video_cover_mobile = models.ImageField(upload_to='video_covers/', verbose_name="Обложка видео, мобильная", blank=True, null=True)
     drug = models.ManyToManyField("pages.Drug", blank=True, null=True, verbose_name='Препарат')
     video_recomendations = models.ManyToManyField('VideoLectures', null=True, blank=True)
     access_number = RichTextField(verbose_name="Поле для добавления расшифровок и номеров одобрения")
     speciality = models.ManyToManyField("pages.Specialty", verbose_name='Специальность')
     content_type = models.CharField(max_length=255, choices=VIDEO_TYPE_CHOICES, verbose_name="Поле для выбора типа контента")
 
-    video_cover_1400px = models.ImageField(upload_to='video_covers/1400px/', verbose_name="Обложка видео 1400px", null=True, blank=True)
-    video_cover_2800px = models.ImageField(upload_to='video_covers/2800px/', verbose_name="Обложка видео 2800px", null=True, blank=True)
-    video_cover_390px = models.ImageField(upload_to='video_covers/390px/', verbose_name="Обложка видео 390px", null=True, blank=True)
-    video_cover_780px = models.ImageField(upload_to='video_covers/780px/', verbose_name="Обложка видео 780px", null=True, blank=True)
+    video_cover_desktop_1400px = models.ImageField(upload_to='video_covers/1400px/', null=True, blank=True)
+    video_cover_desktop_2800px = models.ImageField(upload_to='video_covers/2800px/', null=True, blank=True)
+    recomendation_cover_desktop_430px = models.ImageField(upload_to='video_covers/recomendation/', null=True, blank=True)
+    recomendation_cover_desktop_860px = models.ImageField(upload_to='video_covers/recomendation/', null=True, blank=True)
+
+    video_cover_mobile_420px = models.ImageField(upload_to='video_covers/420px/', null=True, blank=True)
+    video_cover_mobile_840px = models.ImageField(upload_to='video_covers/840px/', null=True, blank=True)
+    recomendation_cover_mobile_270px = models.ImageField(upload_to='video_covers/recomendation/', null=True, blank=True)
+    recomendation_cover_mobile_540px = models.ImageField(upload_to='video_covers/recomendation/', null=True, blank=True)
+
 
     class Meta:
         verbose_name = 'Видео'
@@ -57,8 +66,8 @@ class DisableSignals:
 # Обработчик сигнала pre_save
 @receiver(pre_save, sender=VideoLectures)
 def process_video_cover(sender, instance, **kwargs):
-    if instance.video_cover:
-        file_path = instance.video_cover.path
+    if instance.video_cover_desktop:
+        file_path = instance.video_cover_desktop.path
 
         # Проверяем, существует ли файл
         if Path(file_path).exists():
@@ -69,36 +78,85 @@ def process_video_cover(sender, instance, **kwargs):
                 # Определяем высоту и ширину оригинала
                 original_width, original_height = image.size
 
-                # Задаем высоту для измененных изображений
-                target_height_390 = 390
-                target_height_780 = 780
-                target_height_1400 = 1400
-                target_height_2800 = 2800
+                # Задаем новую ширину для измененных изображений
+                target_width_2800 = 2800
+                target_width_1400 = 1400
+                target_width_430 = 430
+                target_width_860 = 860
 
-                # Рассчитываем новую ширину с сохранением пропорций
-                target_width_390 = int(original_width / original_height * target_height_390)
-                target_width_780 = int(original_width / original_height * target_height_780)
-                target_width_1400 = int(original_width / original_height * target_height_1400)
-                target_width_2800 = int(original_width / original_height * target_height_2800)
+                # Рассчитываем новую высоту с сохранением пропорций
+                target_height_2800 = int(original_height / original_width * target_width_2800)
+                target_height_1400 = int(original_height / original_width * target_width_1400)
+                target_height_430 = int(original_height / original_width * target_width_430)
+                target_height_860 = int(original_height / original_width * target_width_860)
 
                 # Масштабируем изображения с новыми размерами
-                image_stream_390px = BytesIO()
-                image.resize((target_width_390, target_height_390)).save(image_stream_390px, format='JPEG')
-                instance.video_cover_390px.save(f"{instance.video_cover.name}_390px.jpg", File(image_stream_390px), save=False)
-
-                image_stream_780px = BytesIO()
-                image.resize((target_width_780, target_height_780)).save(image_stream_780px, format='JPEG')
-                instance.video_cover_780px.save(f"{instance.video_cover.name}_780px.jpg", File(image_stream_780px), save=False)
+                image_stream_2800px = BytesIO()
+                image.resize((target_width_2800, target_height_2800)).save(image_stream_2800px, format='PNG')
+                instance.video_cover_desktop_2800px.save(f"{instance.video_cover_desktop.name}_2800px.png", File(image_stream_2800px), save=False)
 
                 image_stream_1400px = BytesIO()
-                image.resize((target_width_1400, target_height_1400)).save(image_stream_1400px, format='JPEG')
-                instance.video_cover_1400px.save(f"{instance.video_cover.name}_1400px.jpg", File(image_stream_1400px), save=False)
+                image.resize((target_width_1400, target_height_1400)).save(image_stream_1400px, format='PNG')
+                instance.image_desktop_1400px.save(f"{instance.video_cover_desktop.name}_1400px.png", File(image_stream_1400px), save=False)
 
-                image_stream_2800px = BytesIO()
-                image.resize((target_width_2800, target_height_2800)).save(image_stream_2800px, format='JPEG')
-                instance.video_cover_2800px.save(f"{instance.video_cover.name}_2800px.jpg", File(image_stream_2800px), save=False)
+                image_stream_430px = BytesIO()
+                image.resize((target_width_430, target_height_430)).save(image_stream_430px, format='PNG')
+                instance.recomendation_cover_desktop_430px.save(f"{instance.video_cover_desktop.name}_430px.png", File(image_stream_430px), save=False)
+
+                image_stream_860px = BytesIO()
+                image.resize((target_width_860, target_height_860)).save(image_stream_860px, format='PNG')
+                instance.recomendation_cover_desktop_860px.save(f"{instance.video_cover_desktop.name}_860px.png", File(image_stream_860px), save=False)
 
                 # Сохраняем изменения в модели вручную
                 instance.save()
+
+        else:
+            print(f"Файл не найден: {file_path}")
+
+
+    if instance.video_cover_mobile:
+        file_path = instance.video_cover_mobile.path
+
+        # Проверяем, существует ли файл
+        if Path(file_path).exists():
+            # Отключаем сигналы для избежания рекурсии
+            with DisableSignals(sender=VideoLectures):
+                image = Image.open(file_path)
+
+                # Определяем высоту и ширину оригинала
+                original_width, original_height = image.size
+
+                # Задаем новую ширину для измененных изображений
+                target_width_420 = 420
+                target_width_840 = 840
+                target_width_270 = 270
+                target_width_540 = 540
+
+                # Рассчитываем новую высоту с сохранением пропорций
+                target_height_420= int(original_height / original_width * target_width_420)
+                target_height_840 = int(original_height / original_width * target_width_840)
+                target_height_270 = int(original_height / original_width * target_width_270)
+                target_height_540 = int(original_height / original_width * target_width_540)
+
+                # Масштабируем изображения с новыми размерами
+                image_stream_420px = BytesIO()
+                image.resize((target_width_420, target_height_420)).save(image_stream_420px, format='PNG')
+                instance.video_cover_mobile_420px.save(f"{instance.video_cover_mobile.name}_420px.png", File(image_stream_420px), save=False)
+
+                image_stream_840px = BytesIO()
+                image.resize((target_width_840, target_height_840)).save(image_stream_840px, format='PNG')
+                instance.video_cover_mobile_840px.save(f"{instance.video_cover_mobile.name}_840px.png", File(image_stream_840px), save=False)
+
+                image_stream_270x = BytesIO()
+                image.resize((target_width_270, target_height_270)).save(image_stream_270x, format='PNG')
+                instance.recomendation_cover_mobile_270px.save(f"{instance.video_cover_mobile.name}_270px.png", File(image_stream_270x), save=False)
+
+                image_stream_540px = BytesIO()
+                image.resize((target_width_860, target_height_860)).save(image_stream_860px, format='PNG')
+                instance.recomendation_cover_desktop_860px.save(f"{instance.video_cover_mobile.name}_860px.png", File(image_stream_860px), save=False)
+
+                # Сохраняем изменения в модели вручную
+                instance.save()
+
         else:
             print(f"Файл не найден: {file_path}")
