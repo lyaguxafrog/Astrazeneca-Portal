fa774fd2a5fca1c811744012bf10b7b665bba420
adrian <maccree.a@yandex.ru>
2024-01-09 00:22:23 +0300

    ssl update

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
-certbot certonly --webroot -w /var/www/certbot -d astraportal.dev-demo.online --email maccree.a@yandex.ru --agree-tos
+sleep "${WAIT_FOR_NUXT:-10}"
+
+certbot certonly --webroot -w /var/www/certbot -d astraportal.dev-demo.online --email maccree.a@yandex.ru --agree-tos --non-interactive --force-renewal
 
 exec "$@"
