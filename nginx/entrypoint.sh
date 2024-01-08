e39af9a5fcac93d6d84e6c298feb7eb007a8ab11
adrian <maccree.a@yandex.ru>
2024-01-08 23:16:58 +0300

    email

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-certbot certonly --webroot -w /var/www/certbot -d astraportal.dev-demo.online
+certbot certonly --webroot -w /var/www/certbot -d astraportal.dev-demo.online --email maccree.a@yandex.ru --agree-tos
 
 exec "$@"
