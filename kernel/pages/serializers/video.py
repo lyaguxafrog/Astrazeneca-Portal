0d47d8e0724382311851f0b8bb667352492d083d
adrian <maccree.a@yandex.ru>
2024-01-10 12:26:05 +0300

    нарезка рекомендаций видео

--- kernel/pages/serializers/video.py
+++ kernel/pages/serializers/video.py
@@ -79,10 +79,10 @@ class VideoLecturesSerializer(serializers.ModelSerializer):
             {
                 'id': recommendation.id,
                 'title': recommendation.video_article,
-                'recomendation_cover_desktop_430px': self.get_relative_url(recommendation.recomendation_cover_desktop_430px),
-                'recomendation_cover_desktop_860px': self.get_relative_url(recommendation.recomendation_cover_desktop_860px),
-                'recomendation_cover_mobile_270px': self.get_relative_url(recommendation.recomendation_cover_mobile_270px),
-                'recomendation_cover_mobile_540px': self.get_relative_url(recommendation.recomendation_cover_mobile_540px),
+                'recomendation_cover_desktop_500px': self.get_relative_url(recommendation.recomendation_cover_desktop_500px),
+                'recomendation_cover_desktop_1000px': self.get_relative_url(recommendation.recomendation_cover_desktop_1000px),
+                'recomendation_cover_mobile_280px': self.get_relative_url(recommendation.recomendation_cover_mobile_280px),
+                'recomendation_cover_mobile_560px': self.get_relative_url(recommendation.recomendation_cover_mobile_560px),
 
             }
             for recommendation in obj.video_recomendations.all()
@@ -100,12 +100,13 @@ class VideoLecturesListSerializer(serializers.ModelSerializer):
     video_article_url = serializers.SerializerMethodField()
     video_cover_desktop_1400px = serializers.SerializerMethodField()
     video_cover_desktop_2800px = serializers.SerializerMethodField()
-    recomendation_cover_desktop_430px = serializers.SerializerMethodField()
-    recomendation_cover_desktop_860px = serializers.SerializerMethodField()
+    recomendation_cover_desktop_500px = serializers.SerializerMethodField()
+    recomendation_cover_desktop_1000px = serializers.SerializerMethodField()
     video_cover_mobile_420px = serializers.SerializerMethodField()
     video_cover_mobile_840px = serializers.SerializerMethodField()
-    recomendation_cover_mobile_270px = serializers.SerializerMethodField()
-    recomendation_cover_mobile_540px = serializers.SerializerMethodField()
+    recomendation_cover_mobile_280px = serializers.SerializerMethodField()
+    recomendation_cover_mobile_560px = serializers.SerializerMethodField()
+# Рекомендуемые видео - десктоп: 500px, 1000px, мобилка: 280px, 560px
 
     class Meta:
         model = VideoLectures
@@ -115,12 +116,12 @@ class VideoLecturesListSerializer(serializers.ModelSerializer):
                   'speciality',
                     'video_cover_desktop_1400px',
                     'video_cover_desktop_2800px',
-                    'recomendation_cover_desktop_430px',
-                    'recomendation_cover_desktop_860px',
+                    'recomendation_cover_desktop_500px',
+                    'recomendation_cover_desktop_1000px',
                     'video_cover_mobile_420px',
                     'video_cover_mobile_840px',
-                    'recomendation_cover_mobile_270px',
-                    'recomendation_cover_mobile_540px',
+                    'recomendation_cover_mobile_280px',
+                    'recomendation_cover_mobile_560px',
             ]
 
     def get_video_cover_desktop_1400px(self, obj):
@@ -129,11 +130,11 @@ class VideoLecturesListSerializer(serializers.ModelSerializer):
     def get_video_cover_desktop_2800px(self, obj):
         return self.get_relative_url(obj.video_cover_desktop_2800px)
 
-    def get_recomendation_cover_desktop_430px(self, obj):
-        return self.get_relative_url(obj.recomendation_cover_desktop_430px)
+    def get_recomendation_cover_desktop_500px(self, obj):
+        return self.get_relative_url(obj.recomendation_cover_desktop_500px)
 
-    def get_recomendation_cover_desktop_860px(self, obj):
-        return self.get_relative_url(obj.recomendation_cover_desktop_860px)
+    def get_recomendation_cover_desktop_1000px(self, obj):
+        return self.get_relative_url(obj.recomendation_cover_desktop_1000px)
 
     def get_video_cover_mobile_420px(self, obj):
         return self.get_relative_url(obj.video_cover_mobile_420px)
@@ -141,11 +142,11 @@ class VideoLecturesListSerializer(serializers.ModelSerializer):
     def get_video_cover_mobile_840px(self, obj):
         return self.get_relative_url(obj.video_cover_mobile_840px)
 
-    def get_recomendation_cover_mobile_270px(self, obj):
-        return self.get_relative_url(obj.recomendation_cover_mobile_270px)
+    def get_recomendation_cover_mobile_280px(self, obj):
+        return self.get_relative_url(obj.recomendation_cover_mobile_280px)
 
-    def get_recomendation_cover_mobile_540px(self, obj):
-        return self.get_relative_url(obj.recomendation_cover_mobile_540px)
+    def get_recomendation_cover_mobile_560px(self, obj):
+        return self.get_relative_url(obj.recomendation_cover_mobile_560px)
 
     def get_video_article_url(self, obj):
         return self.get_relative_url(obj.video_article)
