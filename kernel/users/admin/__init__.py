341dd602f410a016e148f39a2eda83d8ba360a9d
adrian <maccree.a@yandex.ru>
2024-01-12 00:26:13 +0300

    фикс видео

--- kernel/users/admin/__init__.py
+++ kernel/users/admin/__init__.py
@@ -3,3 +3,7 @@
 from django.contrib import admin
 
 from .user import UserAdmin, UserProfile, UserProfileInline
+from django.contrib.auth.models import User, Group
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
