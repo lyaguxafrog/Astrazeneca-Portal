64a77a576974def0c588ed7890d40f45dcd49f4c
TimMshk <tima.moshkara@mail.ru>
2024-01-16 17:34:56 +0300

    upd

--- frontend/components/layout/DefaultHistoriesSlider.vue
+++ frontend/components/layout/DefaultHistoriesSlider.vue
@@ -238,13 +238,13 @@ $root: histories-slider;
         &-img {
           width: 100%;
           height: 100%;
+
+          border-width: 2px;
         }
       }
 
       :deep(img) {
         height: 100%;
-
-        border-width: 2px;
       }
     }
 
