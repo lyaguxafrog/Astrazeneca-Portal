faed99418f84ba8aa2d5a9aef11039010e51974e
adrian <maccree.a@yandex.ru>
2024-01-19 05:33:38 +0300

    .env file

--- helper.sh
+++ helper.sh
@@ -5,6 +5,7 @@ if [[ $1 = 'config' ]]; then
 
     cat .env.example >> kernel/.env
     echo ".env создан"
+    echo "Обязательно смените SECRET_KEY и ADMIN_PASSWORD"
 fi
 
 if [[ $1 = 'deploy' ]]; then
@@ -20,8 +21,8 @@ if [[ $1 = 'deploy' ]]; then
     echo "Frontend: Timofey Moshkara @TimofeyMoshkara | Backend: Adrian Makridenko @lyaguxafrog" > .author
 
     docker-compose up -d --build 
-    docker system prune -af
     clear
+    echo "Deployed!"
 fi
 
 if [[ $1 = 'update' ]]; then
@@ -29,6 +30,7 @@ if [[ $1 = 'update' ]]; then
 	git stash
 	git pull
 	docker-compose up -d --build
-
+	docker system prune -af
+	clear
 	exit 0
 fi
