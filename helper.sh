398f8d0e18b00085acaac09dbfb6cf7d96f7fa41
adrian <maccree.a@yandex.ru>
2024-01-25 15:11:30 +0300

    ssl

--- helper.sh
+++ helper.sh
@@ -5,7 +5,9 @@ if [[ $1 = 'config' ]]; then
 
     cat .env.example >> kernel/.env
     echo ".env создан"
-    echo "Обязательно смените SECRET_KEY и ADMIN_PASSWORD"
+    echo "Обязательно смените SECRET_KEY и ADMIN_PASSWORD"   
+    mkdir kernel/logs
+    
 fi
 
 if [[ $1 = 'deploy' ]]; then
@@ -16,12 +18,11 @@ if [[ $1 = 'deploy' ]]; then
     rm -rf README.md
     rm -rf kernel/.devcontainer/
     rm -rf kernel/.vscode/
-    
-    mkdir kernel/logs
+
     echo "Frontend: Timofey Moshkara @TimofeyMoshkara | Backend: Adrian Makridenko @lyaguxafrog" > .author
 
     docker-compose up -d --build 
-    clear
+    # clear
     echo "Deployed!"
 fi
 
