409f2cc7c138e4dc941861df625cd32218051f12
adrian <maccree.a@yandex.ru>
2024-01-29 14:26:32 +0300

    url prox

--- kernel/config/urls.py
+++ kernel/config/urls.py
@@ -8,6 +8,7 @@ from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
 
 
 schema_view = get_schema_view(
@@ -29,14 +30,14 @@ urlpatterns = [
     path('api/', include('users.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('author', RedirectView.as_view(url='https://github.com/lyaguxafrog'), name='github-redirect'),
+    path('', TemplateView.as_view(template_name='frontend.html'), name='frontend'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
-from config.settings import DEBUG
-if DEBUG:
+from config.settings import DOCS
+if DOCS:
     urlpatterns += [
         path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
