a0d5cdb9ef6837c4d020c2c3d5d718ed9a5d1896
lyaguxafrog <maccree.a@yandex.ru>
2024-02-27 18:17:19 +0300

    test urls

--- kernel/config/urls.py
+++ kernel/config/urls.py
@@ -9,6 +9,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+
 
 
 schema_view = get_schema_view(
@@ -25,9 +27,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('pages.urls')),
-    path('api/', include('practics.urls')),
-    path('api/', include('users.urls')),
+    path('api/', csrf_exempt(include('pages.urls'))),
+    path('api/', csrf_exempt(include('practics.urls'))),
+    path('api/', csrf_exempt(include('users.urls'))),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('author', RedirectView.as_view(url='https://github.com/lyaguxafrog'), name='github-redirect'),
     path('', TemplateView.as_view(template_name='index.html'), name='frontend'),
@@ -40,6 +42,6 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 from config.settings import DOCS
 if DOCS:
     urlpatterns += [
-        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        path('swagger/', csrf_exempt(schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')),
+        path('redoc/', csrf_exempt(schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')),
         ]
