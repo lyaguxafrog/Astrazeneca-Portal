0687d51d696005da9d06cdfd53ee367a866714cd
adrian <maccree.a@yandex.ru>
2024-02-28 10:05:53 +0300

    test sso

--- kernel/users/services/autorisation.py
+++ kernel/users/services/autorisation.py
@@ -8,7 +8,7 @@ from decouple import config
 
 def login_link(request):
     source = config('OUR_DOMAIN')
-    next_url = config('REDIRECT_URL')
+    next_url = config('OUR_DOMAIN')
 
     login_url = f"https://sso.az.clients.dobroagency.ru/signup?source={source}&next_url={next_url}"
 
