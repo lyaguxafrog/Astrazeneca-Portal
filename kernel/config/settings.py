bcef5fb2a04501bdaa0d7f13dd86327929d41d47
adrian <maccree.a@yandex.ru>
2024-03-04 17:42:45 +0300

    chore: simplify ALLOWED_HOSTS configuration

--- kernel/config/settings.py
+++ kernel/config/settings.py
@@ -17,9 +17,7 @@ if DEBUG:
     CSRF_COOKIE_SECURE = False
     CSRF_TRUSTED_ORIGINS = ['http://*']
 else:
-    ALLOWED_HOSTS = [os.getenv("OUR_DOMAIN"),
-                     f'{os.getenv("OUR_DOMAIN")}:8000',
-                     f'https://{os.getenv("OUR_DOMAIN")}']
+    ALLOWED_HOSTS = [f'https://{os.getenv("OUR_DOMAIN")}']
 
 
     CSRF_COOKIE_SECURE = True
