3f74dcabdef329f561507b52e3eb04bc67612959
adrian <maccree.a@yandex.ru>
2024-03-06 15:27:26 +0300

    restore models

--- kernel/practics/models/__init__.py
+++ kernel/practics/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# from .practicum import Practicum, Screens
-# from .screen_blocks import (ScreenTextBlock, ScreenImageBlock,
-#                                ScreenPopupBlock, ScreenButton)
+from .practicum import Practicum, Screens
+from .screen_blocks import (ScreenTextBlock, ScreenImageBlock,
+                               ScreenPopupBlock, ScreenButton)
 from .practic_exam import PrTest, AnswerButtons
